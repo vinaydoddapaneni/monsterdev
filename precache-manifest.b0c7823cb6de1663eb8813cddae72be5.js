ae6a7142a059a2e47db6f4657f2a3d778ac96752
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2019-07-29 13:51:22 +0530

    Updates

---
+++ precache-manifest.b0c7823cb6de1663eb8813cddae72be5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd9df99a49019a12333b6ba2ac939e1",
+    "url": "/monsterdev/index.html"
+  },
+  {
+    "revision": "a9a80637f2500e054dd3",
+    "url": "/monsterdev/static/css/main.be41d83d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsterdev/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a9a80637f2500e054dd3",
+    "url": "/monsterdev/static/js/main.684ef4d1.chunk.js"
+  },
+  {
+    "revision": "9d5b03c74cb72546b99a",
+    "url": "/monsterdev/static/js/runtime~main.98116dd2.js"
+  }
+]);
